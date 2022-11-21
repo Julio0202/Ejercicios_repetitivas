8e3d8f1079bb89adf57c47a5e5f5c6cede68fe65
Julio Chen <al_ychen@iessierradelasvillas.org>
2022-11-21 11:11:58 +0100

    Tkinter

--- Tkinter.py
+++ Tkinter.py
@@ -1,18 +1,34 @@
 from tkinter import *
 from tkinter import ttk
 
+def saludar():
+    texto = campoTexto.get()
+    textoLabel.set(texto)
+
 #Generar la ventana
 ventana = Tk()
 ventana.title("Primer ejercicio")
-ventana.geometry("300x100")
+ventana.geometry("500x500")
 ventana.resizable(width=False,height=False)
-ventana.config(background="cum")
+ventana.config(background="black")
+ventana.iconbitmap()
+
 #Generar el lienzo para pintar componentes
-frm = ttk.Frame(ventana, padding=10).pack()
+frm = ttk.Frame(ventana, padding=20).pack()
+
 #Componentes Label y button
-labelTexto = ttk.Label(frm, text = "Hola Tkinter") 
+textoLabel = StringVar()
+textoLabel.set("Hola tkinter")
+labelTexto = ttk.Label(frm, textvariable=textoLabel) 
 labelTexto.config(background="red",border=5,font=("Arial",15))
 labelTexto.pack()
+
+#Componente cuadro de texto 
+campoTexto = ttk.Entry(frm) 
+campoTexto.pack()
+
+
+
+ttk.Button(frm, text="Saludo", command=saludar).pack()
 ttk.Button(frm, text="Salir", command=ventana.destroy).pack()
-#
 ventana.mainloop()
